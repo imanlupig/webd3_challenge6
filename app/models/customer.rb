73aa3ba96301eb350d474f119cf1811028bab4cf
Ian Manlupig <imanlupig@academic.rrc.ca>
2024-10-14 18:13:42 -0500

    removed validation for email_address in Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ApplicationRecord
-  # validations to check for full name and email address
+  # validations to check for full name
   validates :full_name, presence: true
-  validates :email_address, presence: true
+
 
   has_one_attached :image
 end
